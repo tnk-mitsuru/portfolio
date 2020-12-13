661441498f060d67a689e8e11596dc5cbc9b99e1
田中暢 <mitsuru@rrcs-172-254-99-36.nyc.biz.rr.com>
2020-12-13 15:14:51 +0900

    dev postcss

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+/**
+ * PostCSS Config
+ */
+module.exports = (ctx) => {
+  return {
+    map: ctx.options.map,
+    plugins: {
+      'postcss-import': {},
+      'postcss-preset-env': {
+        stage: 3,
+        features: {
+          'nesting-rules': true,
+        },
+        autoprefixer: {
+          cascade: false,
+          grid: 'autoplace'
+        },
+      },
+      'postcss-flexbugs-fixes': {}
+    },
+  }
+}

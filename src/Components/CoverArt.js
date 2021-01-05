3445528b608f3d9dc04961e03529411e443e73d9
MITSURU TANAKA <tnk_mitsuru@yahoo.co.jp>
2021-01-05 10:00:56 +0900

    create slideshow of coverArt

--- src/Components/CoverArt.js
+++ src/Components/CoverArt.js
@@ -1,10 +1,26 @@
+import { Fade } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
 import styles from '../styles/coverArt.module.css';
+
 
 export default function CoverArt() {
   return (
     <div className={styles.coverArt}>
-      <div className={styles.image}>
+      <div className={styles.heading}>
         <h1>Mitsuru's Art</h1>
+      </div>
+      <div className={styles.slideContainer}>
+        <Fade arrows={false} duration={4000}>
+          <div>
+            <div className={styles.image1}></div>
+          </div>
+          <div>
+            <div className={styles.image2}></div>
+          </div>
+          <div>
+            <div className={styles.image3}></div>
+          </div>
+        </Fade>
       </div>
     </div>
   );

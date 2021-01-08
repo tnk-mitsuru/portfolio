06b8e5ea965dc2669b296715914f234080f40575
MITSURU TANAKA <tnk_mitsuru@yahoo.co.jp>
2021-01-09 08:38:36 +0900

    create redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import WebDesign from './Pages/WebDesign';
 import ConceptArt from './Pages/ConceptArt';
@@ -7,14 +7,11 @@ import Copyright from './Components/Copyright';
 export default function App() {
   return (
     <div>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename='/portfolio'>
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route exact path='/WebDesign' component={WebDesign}/>
           <Route exact path='/ConceptArt' component={ConceptArt}/>
-          <Redirect exact from='/WebDesign' to='/WebDesign' />
-          <Redirect exact from='/ConceptArt' to='/ConceptArt' />
-          <Redirect to='/' />
         </Switch>
       </BrowserRouter>
       <Copyright />

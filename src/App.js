30c5f511708aa4d5ab3846c578c52b6789b60806
MITSURU TANAKA <tnk_mitsuru@yahoo.co.jp>
2021-01-08 23:29:49 +0900

    modify router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import WebDesign from './Pages/WebDesign';
 import ConceptArt from './Pages/ConceptArt';
@@ -7,13 +7,13 @@ import Copyright from './Components/Copyright';
 export default function App() {
   return (
     <div>
-      <HashRouter>
+      <BrowserRouter basename="/">
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route exact path='/WebDesign' component={WebDesign}/>
           <Route exact path='/ConceptArt' component={ConceptArt}/>
         </Switch>
-      </HashRouter>
+      </BrowserRouter>
       <Copyright />
     </div>
   );

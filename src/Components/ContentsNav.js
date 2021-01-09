a131cec75ed5e140869468fe1e219dbfcb658362
MITSURU TANAKA <tnk_mitsuru@yahoo.co.jp>
2021-01-10 07:49:21 +0900

    add google analytics

--- src/Components/ContentsNav.js
+++ src/Components/ContentsNav.js
@@ -5,16 +5,16 @@ export default function ContentsNav() {
   return (
     <div className="mainBlock">
       <div className={`mainBlockMargin ${styles.contentsNav}`}>
-        <section className={styles.webDesign}>
-          <Link to="/WebDesign">
-            <div className={styles.contntArt}></div>
-            <div className={styles.heading}><h2>Web Design</h2></div>
-          </Link>
-        </section>
         <section className={styles.conceptArt}>
           <Link to="/ConceptArt">
             <div className={styles.contntArt}></div>
             <div className={styles.heading}><h2>Concept Art</h2></div>
+          </Link>
+        </section>
+        <section className={styles.webDesign}>
+          <Link to="/WebDesign">
+            <div className={styles.contntArt}></div>
+            <div className={styles.heading}><h2>Web Design</h2></div>
           </Link>
         </section>
       </div>

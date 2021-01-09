1e711ddcdf35c14f7ec53fd4846f58c494256a20
MITSURU TANAKA <tnk_mitsuru@yahoo.co.jp>
2021-01-09 13:42:32 +0900

    modify gallery

--- src/Components/GalleryWebDesign.js
+++ src/Components/GalleryWebDesign.js
@@ -5,19 +5,31 @@ import Carousel, { Modal, ModalGateway } from "react-images";
 // import Carousel, { Modal, ModalGateway } from "react-responsive-carousel";
 
 import art1 from '../images/webDesign_wireFrame.png';
-import art2 from '../images/webDesign_moodBoard.png';
+import art2 from '../images/webDesign_titleLogo.png';
+import art3 from '../images/webDesign_moodBoard.png';
+import art4 from '../images/webDesign_colorScheme.png';
 
 export default function GalleryWebDesign () {
     const photos = [
         {
           src: art1,
-          width: 1.8,
+          width: 1,
           height: 1
         },
         {
           src: art2,
-          width: 1.8,
+          width: 2.5,
           height: 1
+        },
+        {
+          src: art3,
+          width: 1.2,
+          height: 1,
+        },
+        {
+          src: art4,
+          width: 2,
+          height: 1,
         }
     ];
     const [currentImage, setCurrentImage] = useState(0);

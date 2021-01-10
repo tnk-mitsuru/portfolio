64a0942c2797167cfca304deffdb7dcd1dac6b5f
MITSURU TANAKA <tnk_mitsuru@yahoo.co.jp>
2021-01-10 19:57:35 +0900

    modify images

--- src/Components/Biography.js
+++ src/Components/Biography.js
@@ -8,7 +8,7 @@ export default function Biography() {
           <table className="tableSimple">
             <tbody>
               <tr><th>2008 - 2010</th><td>東京大学大学院 地球惑星科学専攻</td></tr>
-              <tr><th>2010 - 現在</th><td>大手IT企業 デザイナー</td></tr>
+              <tr><th>2010 - 現在</th><td>IT企業 デザイナー</td></tr>
             </tbody>
           </table>
         </section>

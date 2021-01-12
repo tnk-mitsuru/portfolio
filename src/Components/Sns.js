895dde95a509debeb9b60515b593b43edb79803f
MITSURU TANAKA <tnk_mitsuru@yahoo.co.jp>
2021-01-12 10:20:31 +0900

    modify sns icon code

--- src/Components/Sns.js
+++ src/Components/Sns.js
@@ -1,21 +1,26 @@
 import styles from '../styles/sns.module.css';
 import twitter from '../images/icon_twitter.svg';
 import note from '../images/icon_note.svg';
-import github from '../images/icon_github.svg';
+import github from '../images/icon_instagram.svg';
 
 export default function Sns() {
   return (
     <div className="mainBlock">
-      <div className={`mainBlockMargin ${styles.sns}`}>
-        <div className={styles.account}>
-            <a href="https://twitter.com/artist_mitsuru" target="_blank" rel="noreferrer"><img src={twitter} alt="twitterアカウント" width="24"/>@artist_mitsuru</a>
-        </div>
-        <div className={styles.account}>
-            <a href="https://note.com/artist_mitsuru" target="_blank" rel="noreferrer"><img src={note} alt="noteアカウント" width="24"/>@artist_mitsuru</a>
-        </div>
-        <div className={styles.account}>
-            <a href="https://github.com/tnk-mitsuru" target="_blank" rel="noreferrer"><img src={github} alt="githubアカウント" width="24"/>@tnk-mitsuru</a>
-        </div>
+      <div className="mainBlockMargin">
+        <section>
+          <h2 className="headingH2">Contact &amp; SNS</h2>
+          <ul className={styles.accounts}>
+            <li>
+                <a href="https://twitter.com/artist_mitsuru" target="_blank" rel="noreferrer"><img src={twitter} alt="twitterアカウント"/>@artist_mitsuru</a>
+            </li>
+            <li className={styles.account}>
+                <a href="https://note.com/artist_mitsuru" target="_blank" rel="noreferrer"><img src={note} alt="noteアカウント"/>@artist_mitsuru</a>
+            </li>
+            <li className={styles.account}>
+                <a href="https://www.instagram.com/artist.mitsuru/" target="_blank" rel="noreferrer"><img src={github} alt="Instagramアカウント"/>@tnk-mitsuru</a>
+            </li>
+          </ul>
+        </section>
       </div>
     </div>
   );

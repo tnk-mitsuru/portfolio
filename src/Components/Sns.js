4d67001a749410f0f0ed23123c92f1ca42634470
MITSURU TANAKA <tnk_mitsuru@yahoo.co.jp>
2021-06-18 07:46:45 +0900

    modify contact

--- src/Components/Sns.js
+++ src/Components/Sns.js
@@ -3,6 +3,7 @@ import twitter from '../images/icon_twitter.svg';
 import instagram from '../images/icon_instagram.png';
 import pixiv from '../images/icon_pixiv.png';
 import youtube from '../images/icon_youtube.png';
+import github from '../images/icon_github.svg';
 
 export default function Sns() {
   return (
@@ -15,13 +16,10 @@ export default function Sns() {
                 <a href="https://www.youtube.com/c/MitsuruArt" target="_blank" rel="noreferrer"><img src={youtube} alt="YouTubeチャンネル"/>作品の制作動画</a>
             </li>
             <li>
-                <a href="https://www.instagram.com/artist.mitsuru/" target="_blank" rel="noreferrer"><img src={instagram} alt="Instagramアカウント"/>油絵などの作品集</a>
-            </li>
-            <li>
                 <a href="https://www.pixiv.net/users/55276850" target="_blank" rel="noreferrer"><img src={pixiv} alt="pixivアカウント"/>ファンアート</a>
             </li>
             <li>
-                <a href="https://twitter.com/artist_mitsuru" target="_blank" rel="noreferrer"><img src={twitter} alt="twitterアカウント"/>作品の宣伝、つぶやきなど</a>
+                <a href="https://github.com/tnk-mitsuru" target="_blank" rel="noreferrer"><img src={github} alt="githubアカウント"/>GitHub</a>
             </li>
           </ul>
         </section>
